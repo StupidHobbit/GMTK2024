acee912ed622936e2b40bd5722d6a1251c1b9051
StupidHobbit <3mir33@gmail.com>
2024-08-18 11:24:23 +0300

    Added simple checkpoint system and death from the floor

--- scenes/components/HealthComponent.gd
+++ scenes/components/HealthComponent.gd
@@ -5,14 +5,14 @@ signal took_damage(damage: int)
 signal health_depleted()
 
 @export var max_hp: int
-var current_hp: int
+var current_hp: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_hp = max_hp
 
-func take_damage(damage: int):
-	current_hp = clampi(current_hp - damage, 0, max_hp)
+func take_damage(damage: float):
+	current_hp = clampf(current_hp - damage, 0, max_hp)
 	took_damage.emit(damage)
 	if current_hp == 0:
 		health_depleted.emit()
