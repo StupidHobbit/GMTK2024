d0584b458ceab058c6f9ce5b2cce1f4436067072
leroymilo <adriaanboxmail@gmail.com>
2024-08-20 15:50:11 +0900

    added invisible wall for big level

--- scenes/lod_collisions.gd
+++ scenes/lod_collisions.gd
@@ -1,15 +1,29 @@
 extends StaticBody3D
 
-@export var shapes: Array[Shape3D] = []
+var LOD_shapes: Array[Node] = []
 
-@onready var shape_node: CollisionShape3D = $CollisionShape3D
+@onready var shape_node: Node3D = $CurrentShape
 
 func _ready() -> void:
-	if shapes.size() == 0:
-		print("LOD Collision Shape has no shape!")
+	if get_child_count() == 0:
+		print("LOD Collision Shape has no children!")
 		return
+	
+	for child in get_children():
+		if child == shape_node: continue
+		LOD_shapes.append(child)
+		remove_child(child)
+	
 	update_shape()
 	Globals.add_scale_watcher(update_shape)
 
 func update_shape() -> void:
-	shape_node.shape = shapes[min(Globals.scale_index, shapes.size()-1)]
+	var id: int = Globals.scale_index
+	
+	for child in shape_node.get_children():
+		shape_node.remove_child(child)
+	
+	if id >= LOD_shapes.size():
+		return
+	
+	shape_node.add_child(LOD_shapes[id])
